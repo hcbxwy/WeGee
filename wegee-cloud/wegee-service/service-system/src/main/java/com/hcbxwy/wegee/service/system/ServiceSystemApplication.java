b649e87fa15ca0e11002dc1774430718bba0a2cd
Billson <billson0618@163.com>
2019-08-19 07:10:22 +0800

    新增系统服务空项目

---
+++ wegee-cloud/wegee-service/service-system/src/main/java/com/hcbxwy/wegee/service/system/ServiceSystemApplication.java
@@ -0,0 +1,19 @@
+package com.hcbxwy.wegee.service.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统服务
+ *
+ * @author Billson
+ * @since 2019-08-18 12:20
+ */
+@SpringBootApplication
+public class ServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSystemApplication.class, args);
+	}
+
+}
