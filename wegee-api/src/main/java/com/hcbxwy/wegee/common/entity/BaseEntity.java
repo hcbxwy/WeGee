fa29f0fbc7b8c8ec3c70c1ac0c2a78dd7daa0712
Billson <huangchaobiao@zhiyingwl.com>
2019-10-17 17:32:11 +0800

    修改ID生成策略为数据库自增

--- wegee-api/src/main/java/com/hcbxwy/wegee/common/entity/BaseEntity.java
+++ wegee-api/src/main/java/com/hcbxwy/wegee/common/entity/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.hcbxwy.wegee.common.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,6 +14,7 @@ import java.util.Date;
  */
 @Data
 public class BaseEntity {
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private Date createTime;
     private Date updateTime;
